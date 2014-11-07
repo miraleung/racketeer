435baab629dfa47a4c0bfb02c7a41648b2dd49de
mira <lngm1@hotmail.com>
2014-11-06 21:38:54 -0800

    runs tests, bad-syntax handling; highlighting needs fixing

--- tool.rkt
+++ tool.rkt
@@ -22,92 +22,123 @@
 
 ; Doesn't work either
 #;
-(define pass-styledelta 
+(define pass-styledelta
   (local [(define blacktextdelta (send bolddelta set-delta-foreground "black"))]
     (send blacktextdelta set-delta-background "green")))
 #;
-(define fail-styledelta 
+(define fail-styledelta
   (local [(define whitetextdelta (send bolddelta set-delta-foreground "white"))]
     (send whitetextdelta set-delta-background "red")))
 
-(define secret-key "(test 1 1)")
-(define secret-key2 "(test 1 2)")
+(define (boolean->string b) (if b "#t" "#f"))
+
+(define secret-key "(test (+ 2 1) (+ 1 2))")
+(define secret-key2 "(test (+ 2 1) (+ 1 1))")
+(define test-start "(test")
+(define test-length 50)
 
 (define tool@
   (unit
     (import drracket:tool^)
     (export drracket:tool-exports^)
-    
+
     (define easter-egg-mixin
       (mixin ((class->interface text%)) ()
-        
+
         (inherit begin-edit-sequence
                  end-edit-sequence
                  insert
                  change-style
                  get-text
                  set-styles-sticky)
-        
+
+        ;; TODO: Get start to be the starting index of the new line.
+        ;; TODO: Stop the highlighting at the end of a test line.
         (define/augment (on-insert start len)
           (begin-edit-sequence))
         (define/augment (after-insert start len)
-          (check-range (max 0 (- start (string-length secret-key)))
+          (check-range (max 0 (- start test-length))
                        (+ start len))
           (end-edit-sequence))
-        
+
         (define/augment (on-delete start len)
           (begin-edit-sequence))
         (define/augment (after-delete start len)
-          (check-range (max 0 (- start (string-length secret-key)))
+          (check-range (max 0 (- start test-length))
                        start)
           (end-edit-sequence))
-        
+
         (define/augment (on-change-style start len)
           (begin-edit-sequence))
         (define/augment (after-change-style start len)
-          (check-range (max 0 (- start (string-length secret-key)))
+          (check-range (max 0 (- start test-length))
                        start)
           (end-edit-sequence))
-        
+
         (define/private (check-range start stop)
           (let/ec k
             (for ((x (in-range start stop)))
               (define after-x
-                (get-text x (+ x (string-length secret-key))))
-                (when (or (string=? after-x secret-key) (string=? after-x secret-key2)) 
-		     (change-style (if (test-passes? after-x) 
-                       ; This has to be inline - if it's defined at the top, styledelta will be set statically.
-                       (send (send bolddelta set-delta-foreground "black") set-delta-background "green")
-                       (send (send bolddelta set-delta-foreground "white") set-delta-background "red"))
-                           x (+ x (string-length secret-key)))))))
-        
+                (get-text x (+ x test-length)))
+                (when (done-test? after-x)
+                  (local [(define test-rc (test-passes? after-x))]
+                    (change-style
+                      ; This has to be inline - if it's defined at the top, styledelta will be set statically.
+                         (cond  [(void? test-rc) ; syntax error
+                                 (send
+                                  (send bolddelta ; (make-object style-delta% 'change-weight 'base)
+                                        set-delta-foreground "black")
+                                   set-delta-background "yellow")]
+                                [test-rc ; pass
+                                  (send
+                                    (send bolddelta set-delta-foreground "black")
+                                  set-delta-background "green")]
+                               [(not test-rc) ; fail
+                                (send
+                                  (send bolddelta set-delta-foreground "white")
+                                  set-delta-background "red")])
+                               x (+ x (string-length (newline-begins-substring after-x)))))
+                  ;; fyi: this has no effect
+                  #;
+                  (send
+                    (send (make-object style-delta% 'change-weight 'base) set-delta-foreground "black")
+                    set-delta-background "white")
+                  ))))
+
         (super-new)))
-    
-    ; string -> boolean
+
+    ; string -> (or/c 1 0 -1)
     ; Takes a string containing a test expression, i.e. "(test exp1 exp2)"
     ; and returns #t if exp1 and exp2 evaluate to the same value.
     ; TODO: handle exceptions
-    (define (test-passes? str) 
+    (define (test-passes? str)
       (define test-exp (read (open-input-string str)))
       (match test-exp
-        [(list 'test expected actual) (equal? (eval expected ns) (eval actual ns))]
-        [else #f]))
-    
+        [(list 'test expected actual)
+         (local [(define (try-eval expr)
+                   (with-handlers [(exn:fail? (lambda (e) (void)))] (eval expr ns)))
+                 (define chk (try-eval expected))
+                 (define ept (try-eval actual))]
+                (if (or (void? chk) (void? ept))
+                  (void)
+                  (equal? chk ept)))]
+         [else #f]))
+
 ;; string -> boolean
 ;; Returns true if str is a test-expression with matching closed parentheses.
 ;; Does not check for syntax of internal expressions.
 ;; TODO: Abstract to {}, [].
-(define (done-test? str) 
+(define (done-test? str)
   (and (> (string-length str) 5)
        (string=? (substring str 0 5) "(test")
        (parens-closed? str)))
 
 ;; string -> boolean
-;; Returns true if str starts with an open paren and 
+;; Returns true if str starts with an open paren and
 ;; has a matching number of parens.
 (define (parens-closed? str)
   (and (> (string-length str) 0)
-       (string=? (substring str 0 1) ")")
+       (string=? (substring str 0 1) "(")
        (parens-closed-helper str 0 0)))
 
 ;; string Int Int -> boolean
@@ -119,7 +150,65 @@
         [(string=? (substring str 0 1) ")")
          (parens-closed-helper (substring str 1) l (+ r 1))]
         [else (parens-closed-helper (substring str 1) l r)]))
-    
+
+;; list list -> boolean
+;; Returns true if l1 is equal to l2
+(define (list-equals? l1 l2)
+  (and (= (length l1) (length l2))
+       (or (and (empty? l1) (empty? l2))
+           (and (= (first l1) (first l2)) (list-equals? (rest l1) (rest l2))))))
+
+;; Returns the index of a character in the given list
+;; e.g. (indexof #\newline (string->list "adsf
+;        (test"))) -> 4
+(define (indexof elem lst)
+  (indexof-h elem lst char=? 0))
+
+;; erm... Haskell notation since not sure of the Racket equivalent
+;; a -> [a] -> (a -> Bool) -> Int -> Int
+;; Helper for indexof ^
+;; Returns -1 if element can't be found
+(define (indexof-h elem lst pred acc)
+  (if (empty? lst)
+    -1
+    (if (pred (first lst) elem)
+      acc
+      (indexof-h elem (rest lst) pred (add1 acc)))))
+
+;; string -> string
+;; Returns the string from the first newline
+;; e.g. (newline-begins-substring "asdf
+;        (test") -> "(test"
+(define (newline-begins-substring str)
+  (define txt-list (string->list str))
+  (define mbr (member #\newline txt-list))
+  (if (not mbr)
+    str
+    (list->string (drop mbr 1))))
+
+;; string -> string
+;; Returns line between two newlines
+;; e.g. (newline-between "adsf
+;        (test
+;        qwerty") -> "(test"
+(define (newline-between str)
+  (local [(define newline-substr-1 (newline-begins-substring str))
+          (define newline-idx-2 (indexof #\newline (string->list newline-substr-1)))]
+         (if (< newline-idx-2 0)
+           newline-substr-1
+           (substring newline-substr-1 0 newline-idx-2))))
+
+;; list list -> boolean
+;; Returns true if l1 is a subsequence of l2
+;; e.g. (subseq? '(1 2 3) '(5 1 2 3 4)) -> #t
+;       (subseq? '() '(1 2 3 4)) -> #t
+;       (subseq? '(1 2 3) '(1 2 4 3)) -> #f
+(define (subseq? l1 l2)
+  (cond [(empty? l1) #t] ; not necessary
+        [(= (first l1) (first l2)) (list-equals? l1 (take l2 (length l1)))]
+        [else (and (>= (length l2) (length l1)) (subseq? l1 (rest l2)))]))
+
+
 ;; PLUGIN FUNCTIONS ==============
     (define reverse-button-mixin
       (mixin (drracket:unit:frame<%>) ()
@@ -127,7 +216,7 @@
         (inherit get-button-panel
                  get-definitions-text)
         (inherit register-toolbar-button)
-        
+
         (let ((btn
                (new switchable-button%
                     (label "Reverse Definitions")
@@ -140,7 +229,7 @@
           (send (get-button-panel) change-children
                 (λ (l)
                   (cons btn (remq btn l)))))))
-    
+
     (define reverse-content-bitmap
       (let* ((bmp (make-bitmap 16 16))
              (bdc (make-object bitmap-dc% bmp)))
@@ -153,7 +242,7 @@
         (send bdc draw-ellipse 6 6 8 8)
         (send bdc set-bitmap #f)
         bmp))
-    
+
     (define (reverse-content text)
       (for ((x (in-range 1 (send text last-position))))
         (send text split-snip x))
@@ -168,9 +257,9 @@
           snip))
       (for ((x (in-list released-snips)))
         (send text insert x 0 0)))
-    
+
     (define (phase1) (void))
     (define (phase2) (void))
-    
+
     (drracket:get/extend:extend-definitions-text easter-egg-mixin)
     (drracket:get/extend:extend-unit-frame reverse-button-mixin)))

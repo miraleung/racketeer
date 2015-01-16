f01ce94776a2c16da6663233dbfb29b1ff64022a
mira <lngm1@hotmail.com>
2015-01-16 01:12:07 -0800

    Merge devel branch commits up to fc43bd42d67

--- tool.rkt
+++ tool.rkt
@@ -1,291 +1,65 @@
-#lang racket
+#lang racket/unit
 
-;; CPSC 311 2014W1
 ;; Racketeer (final version)
 ;; Continuous Testing in DrRacket
-
 (require drracket/tool
-         drracket/tool-lib
-         racket/class
-         racket/gui/base
-         racket/gui
-         racket/unit
-         racket/port
-         racket/match
-         mrlib/switchable-button
-         test-engine/racket-tests
-         racket/sandbox
          framework
-         unstable/function 
-         wxme)
-(provide tool@)
+         racket/class
+         racket/gui
+         unstable/function
+         "private/testing-mixin.rkt")
 
-; Hook into current namespace; for eval.
-(define-namespace-anchor anc)
-(define ns (namespace-anchor->namespace anc))
+(import drracket:tool^)
+(export drracket:tool-exports^)
 
-;; Test/error structs
-(define-struct passed-test (void))
-(define-struct failed-test (void))
-(define-struct syntax-error (void))
-(define-struct out-of-memory-error (void))
-(define-struct other-error (void)) ; meant for exceptions
+;; Statusbar ==============
+(define racketeer-frame-mixin
+  (mixin (drracket:unit:frame<%>) ()
 
-;(define bolddelta (make-object style-delta% 'change-weight 'bold))
-(define pass-delta (send (send (make-object style-delta% 'change-weight 'bold) set-delta-foreground "black") set-delta-background "green"))
-(define fail-delta (send (send (make-object style-delta% 'change-weight 'bold) set-delta-foreground "white") set-delta-background "red"))
-(define syn-error-delta (send (send (make-object style-delta% 'change-weight 'bold) set-delta-foreground "black") set-delta-background "yellow"))
-(define oom-error-delta (send (send (make-object style-delta% 'change-weight 'bold) set-delta-foreground "white") set-delta-background "blue"))
-(define oth-error-delta (send (send (make-object style-delta% 'change-weight 'bold) set-delta-foreground "white") set-delta-background "purple"))
-;(send bolddelta set-weight-on 'bold)
-;(send bolddelta set-weight-off 'normal)
+    (inherit get-definitions-text)
+
+    ;; Definitions for the status bar.
+    ;; Based on panel for drracket-vim-tool (github.com/takikawa/drracket-vim-tool/blob/master/tool.rkt)
+    (define rktr-status-parent-panel  'uninitialized)
+    (define rktr-status-panel         'uninitialized)
+    (define rktr-status-message       'uninit)
+
+    (define/override (file-menu:between-open-and-revert file-menu)
+      (super file-menu:between-open-and-revert file-menu)
+      (new checkable-menu-item%
+           [label "Racketeer Test Highlighting"]
+           [parent file-menu]
+           [callback
+             (lambda (i e) (send (get-definitions-text) toggle-highlight!))]
+           [checked (send (get-definitions-text) highlight?)]))
+
+    ;; Construct the frame.
+    (define/override (make-root-area-container cls parent)
+      (set! rktr-status-parent-panel
+            (super make-root-area-container vertical-panel% parent))
+      (define root (new cls [parent rktr-status-parent-panel]))
+      (set! rktr-status-panel
+            (new horizontal-panel%
+                 [style '(border)]
+                 [stretchable-height #f]
+                 [parent rktr-status-parent-panel]))
+      (set! rktr-status-message
+            (new message%
+                 [parent rktr-status-panel]
+                 [auto-resize #t]
+                 [label "text will go here"]))
+      root)
+
+    ;; Message handler.
+    (define/public (set-rktr-status-message str)
+                   (send rktr-status-message set-label str))
+
+    (super-new)))
 
 
-(define (boolean->string b) (if b "#t" "#f"))
-(define pass-test-msg "; yay, it passed")
-(define fail-test-msg "; yay, it failed")
-(define syn-error-msg "; < syntax error")
-(define oom-error-msg "; out-of-memory ")
-(define oth-error-msg "; error occurred ")
-(define test-msg-len (string-length pass-test-msg))
+(define (phase1) (void))
+(define (phase2) (void))
 
-
-(define test-length 70)
-;; TODO: Test keyword support is currently not abstract enough.
-
-(define tool@
-  (unit
-    (import drracket:tool^)
-    (export drracket:tool-exports^)
-    
-    (define easter-egg-mixin
-      (mixin ((class->interface text%) editor<%>) ()
-        
-        (inherit begin-edit-sequence
-                 end-edit-sequence
-                 insert
-                 change-style
-                 find-newline
-                 last-position
-                 line-start-position
-                 line-end-position
-                 get-text
-                 set-styles-sticky
-                 save-port)
-        
-        (define/augment (on-insert start len)
-          (begin-edit-sequence))
-        (define/augment (after-insert start len)
-          (check-range start (+ start len) "after-insert")
-          (end-edit-sequence))
-        
-        (define/augment (on-delete start len)
-          (begin-edit-sequence))
-        (define/augment (after-delete start len)
-          (check-range start (+ start len) "after-delete")
-          (end-edit-sequence))
-        
-        (define/augment (on-load-file loaded? format)
-          (begin-edit-sequence))
-        
-        (define/augment (after-load-file loaded?)
-          (check-range 0 (last-position) "after-load-file") 
-          (end-edit-sequence))
-        
-        (define/private (check-range start stop handler)
-          (let/ec k
-            (define src-out-port (open-output-bytes))
-            (save-port src-out-port)
-            ; Ignore events that trigger for the entire file.
-            (when (not (and (= 0 start) (= (last-position) stop))) 
-              (with-handlers [(exn:fail? (lambda (e) #f))] 
-                (define test-in-port (open-input-bytes (get-output-bytes src-out-port)))
-                (define eval-in-port (open-input-bytes (get-output-bytes src-out-port)))
-                ; Ignore events for WXME-formatted files.
-                ; TODO: Handle WXME files.
-                (when (and (not (is-wxme-stream? test-in-port)) (not (is-wxme-stream? eval-in-port)))
-                  ; If anything is written to the error port while creating the evaluator, write it to a string port.
-                  ; TODO: Write the contents of the string port to the status bar.
-                  (define test-output (open-output-string))
-                  (define evaluator (parameterize [(sandbox-eval-limits '(1 20))
-                                                   (current-error-port test-output)]
-                                      (make-module-evaluator eval-in-port)))
-                  ; The value of evaluator is #f if the source code has syntax errors.
-                  (when evaluator
-                    (define tests (get-tests test-in-port))
-                    (for ([test-syn tests])
-                      (define test-start (max 0 (- (syntax-position test-syn) 1)))
-                      (define test-end (+ (syntax-position test-syn) (syntax-span test-syn)))
-                      (define test-rc (test-passes? test-syn evaluator))
-                      (define test-msg (get-test-msg test-rc))
-                      #;(message-box "test" (string-append "Testing:\n" (get-text test-start test-end) "\n\nResult:\n" test-msg))
-                      (change-style
-                       ; This has to be inline - if it's defined at the top, styledelta will be set statically.
-                       (cond  [(syntax-error? test-rc) syn-error-delta]
-                              [(other-error? test-rc) oth-error-delta]
-                              [(out-of-memory-error? test-rc) oom-error-delta]
-                              [(passed-test? test-rc) pass-delta]
-                              [(failed-test? test-rc) fail-delta])
-                       test-start test-end))))))))
-        (super-new)))
-    
-    (define test-statements (list 'test 'test/exn 'test/pred 'check-error 'check-expect))
-    
-    (define (get-tests src-port)
-      (expand-syntax (get-syntax-list src-port)))
-    
-    (define (get-syntax-list src-port)
-      (local [(define (get-syntax-list-helper src-port syntax-list)
-                (local [(define syn (parameterize [(read-accept-reader #t)
-                                                   (read-accept-lang #t)] 
-                                      (read-syntax 'program src-port)))]
-                  (if (syntax? syn) 
-                      (get-syntax-list-helper src-port (cons syn syntax-list))
-                      syntax-list)))]
-        (get-syntax-list-helper src-port empty)))
-    
-    (define (expand-syntax to-expand)
-      (local [(define (expand-syntax-helper to-expand syntax-list)
-                (if (empty? to-expand) 
-                    syntax-list
-                    (local [(define expanded (syntax->list (first to-expand)))]
-                      (if expanded
-                          (if (test-syntax? (first to-expand))
-                              (expand-syntax-helper (rest to-expand) (cons (first to-expand) syntax-list))
-                              (expand-syntax-helper (append (filter syntax? expanded) (rest to-expand)) syntax-list))
-                          (expand-syntax-helper (rest to-expand) syntax-list)))))]
-        (expand-syntax-helper to-expand empty)))
-    
-    (define (test-syntax? syn)
-      (local [(define expr (syntax->datum syn))]
-        (and (list? expr) 
-             (not (empty? expr)) 
-             (member (first expr) test-statements))))
-    
-    
-    
-    ; syntax -> (or/c boolean void)
-    ; Takes a syntax object containing a test expression, i.e. "(test exp1 exp2)"
-    ; Returns #t if exp1 and exp2 evaluate to the same value, #f if not,
-    ; and (void) if either of exp1 or exp2 have bad syntax.
-    (define (test-passes? test-syn evaluator)
-      (define defn-evaluator 
-        (with-handlers [(exn:fail:syntax? (lambda (e) syn-err))
-                        (exn:fail:out-of-memory? (lambda (e) oom-err))
-                        ; workaround for exn:fail:out-of-memory? not terminating
-                        (exn:fail:resource? (lambda (e) oom-err))
-                        (exn:fail? (lambda (e) other-err))]
-          evaluator))
-      (define test-exp 
-        (with-handlers [ (exn:fail:syntax? (lambda (e) syn-err))
-                         (exn:fail:out-of-memory? (lambda (e) oom-err))
-                         ; workaround for exn:fail:out-of-memory? not terminating
-                         (exn:fail:resource? (lambda (e) oom-err))
-                         (exn:fail? (lambda (e) other-err))]
-          (syntax->datum test-syn)))
-      (define syn-err (make-syntax-error (void)))
-      (define oom-err (make-out-of-memory-error (void)))
-      (define other-err (make-other-error (void)))
-      (define passd-test (make-passed-test (void)))
-      (define faild-test (make-failed-test (void)))
-      (define (try-eval expr)
-        (with-handlers [ ;TODO: Need a good syntax matcher
-                        (exn:fail:syntax? (lambda (e) syn-err))
-                        ; workaround for exn:fail:out-of-memory? not terminating
-                        (exn:fail:resource? (lambda (e) oom-err))
-                        (exn:fail? (lambda (e) other-err))]
-          (with-limits 0.2 0.1 (defn-evaluator expr))))
-      (define (test-eq actual expected)
-        (local [(define actual-val (try-eval actual))
-                (define expected-val (try-eval expected))]
-          (cond [(or (syntax-error? actual-val) (syntax-error? expected-val)) syn-err] ; syntax error
-                [(or (out-of-memory-error? actual-val) (out-of-memory-error? expected-val)) oom-err]    ; some other error
-                [(or (other-error? actual-val) (other-error? expected-val)) other-err]
-                [else (if (eq? actual-val expected-val)
-                          passd-test
-                          faild-test)])))
-      (match test-exp
-        [(list (or 'check-expect 'test) actual expected)
-         (test-eq actual expected)]
-        [(list 'test/exn actual str)
-         (if (not (string? str))
-             syn-err
-             (if (and (string? str) (other-error? (try-eval actual))) ; <- this should be other-error, once syntax-matching is fixed.
-                 passd-test
-                 faild-test))]
-        [(list 'check-error actual)
-         (if (syntax-error? (try-eval actual))
-             passd-test
-             faild-test)]
-        [(list 'test/pred expr pred)
-         (local [(define pred-app (try-eval pred))]
-           (if (not (procedure? pred-app))
-               syn-err
-               (if (pred-app expr)
-                   passd-test
-                   faild-test)))]
-        [else syn-err])) ;; TODO: Change this to other-error ??
-    
-    (define (get-test-msg test-rc)
-      (cond [(syntax-error? test-rc) syn-error-msg]
-            [(out-of-memory-error? test-rc) oom-error-msg]
-            [(other-error? test-rc) oth-error-msg]
-            [(passed-test? test-rc) pass-test-msg]
-            [(failed-test? test-rc) fail-test-msg]))
-    
-    
-    
-    ;; PLUGIN FUNCTIONS ==============
-    (define reverse-button-mixin
-      (mixin (drracket:unit:frame<%>) ()
-        (super-new)
-        (inherit get-button-panel
-                 get-definitions-text)
-        (inherit register-toolbar-button)
-        
-        (let ((btn
-               (new switchable-button%
-                    (label "Reverse Definitions")
-                    (callback (lambda (button)
-                                (reverse-content
-                                 (get-definitions-text))))
-                    (parent (get-button-panel))
-                    (bitmap reverse-content-bitmap))))
-          (register-toolbar-button btn #:number 11)
-          (send (get-button-panel) change-children
-                (lambda (l)
-                  (cons btn (remq btn l)))))))
-    
-    (define reverse-content-bitmap
-      (let* ((bmp (make-bitmap 16 16))
-             (bdc (make-object bitmap-dc% bmp)))
-        (send bdc erase)
-        (send bdc set-smoothing 'smoothed)
-        (send bdc set-pen "black" 1 'transparent)
-        (send bdc set-brush "blue" 'solid)
-        (send bdc draw-ellipse 2 2 8 8)
-        (send bdc set-brush "red" 'solid)
-        (send bdc draw-ellipse 6 6 8 8)
-        (send bdc set-bitmap #f)
-        bmp))
-    
-    (define (reverse-content text)
-      (for ((x (in-range 1 (send text last-position))))
-        (send text split-snip x))
-      (define snips
-        (let loop ((snip (send text find-first-snip)))
-          (if snip
-              (cons snip (loop (send snip next)))
-              '())))
-      (define released-snips
-        (for/list ((snip (in-list snips))
-                   #:when (send snip release-from-owner))
-          snip))
-      (for ((x (in-list released-snips)))
-        (send text insert x 0 0)))
-    
-    (define (phase1) (void))
-    (define (phase2) (void))
-    
-    (drracket:get/extend:extend-definitions-text easter-egg-mixin)
-    (drracket:get/extend:extend-unit-frame reverse-button-mixin)))
+(preferences:set-default 'drracket:racketeer-highlight-tests? #t boolean?)
+(drracket:get/extend:extend-definitions-text racketeer-testing-mixin)
+(drracket:get/extend:extend-unit-frame racketeer-frame-mixin)
